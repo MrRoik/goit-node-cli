c3536dc3117755d7156fc068495dd914fa3d0fcc
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-03-25 17:02:53 +0200

    update rep
    pritter code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { program } from "commander";
-import {listContacts, getContactById, removeContact, addContact } from "./contacts.js"
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
 
 program
   .option("-a, --action <type>", "choose action")
@@ -15,23 +20,23 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-        const contactsArr = await listContacts();
-        console.table(contactsArr);
+      const contactsArr = await listContacts();
+      console.table(contactsArr);
       break;
 
     case "get":
-        const contactObj = await getContactById(id);
-        console.table(contactObj);
+      const contactObj = await getContactById(id);
+      console.table(contactObj);
       break;
 
     case "add":
-        const contactAdd = await addContact(name, email, phone);
-        console.log(contactAdd);
+      const contactAdd = await addContact(name, email, phone);
+      console.log(contactAdd);
       break;
 
     case "remove":
-        const contactDel = await removeContact(id);
-        console.log(contactDel);
+      const contactDel = await removeContact(id);
+      console.log(contactDel);
       break;
 
     default:
